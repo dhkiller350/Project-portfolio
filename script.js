d74d7cf09253057b30a0959fa584e4d122d0ab22
dhkiller350 <100392451+dhkiller350@users.noreply.github.com>
2024-11-30 11:31:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 function redirectTo(url) {
     window.location.href = url;
 }
+
+function customAlert(message) {
+            const originalTitle = document.title;
+            const customTitle = "Welcome!";
+            
+            // Change the title to create a "titled alert box" effect
+            document.title = customTitle;
+            
+            // Show the alert box
+            alert("Welcome to my projects");
+
+            // Revert the title back to the original after the alert is dismissed
+            document.title = originalTitle;
+        }
